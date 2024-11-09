73327c2f0b360914f6a1c7833e8d5fad1224e73b
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-08 19:03:46 -0500

    Solutions to 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    output = "";
+    for (i = 0; i < num; i++) {
+        output += string;
+    }
 
+    return output;
 };
 
 // Do not edit below this line
