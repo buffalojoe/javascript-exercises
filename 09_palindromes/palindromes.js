1724341b256b030ccb3e3a4f5dad639c3da140d5
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-09 13:13:14 -0500

    Completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function(word) {
 
+    const letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+    wordArray = word.toLowerCase().split("");
+    wordArray = wordArray.filter(letter => letterArray.includes(letter));
+    originalWord = wordArray.join("");
+
+    reversedWord = ""
+    for (i = originalWord.length - 1; i >= 0; i--) {
+        reversedWord += originalWord[i];
+    }
+    console.log("Reversed word: " + reversedWord)
+
+    if (originalWord == reversedWord) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(palindromes('0402340534Race car'));
 // Do not edit below this line
 module.exports = palindromes;
