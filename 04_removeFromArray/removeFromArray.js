ab4de78181259699c31b2b6f83d037b016abfea9
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-09 11:34:34 -0500

    Completed removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...inputs) {
+    
+    for (let input of inputs) {
+        if (array.includes(input)) {
+            do {
+                array.splice(array.indexOf(input), 1);
+            } while (array.includes(input));
+        }       
+    }
+       
+    console.log(array);
+    return array;
+    
 };
 
+removeFromArray([1, 3, 3, 5], 1, 5);
 // Do not edit below this line
 module.exports = removeFromArray;
