aa9b960f8c30f051acb597e579ef72ee902812a2
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-09 12:20:13 -0500

    Completed calculator.js exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let answer = 0;
+  for (let num of array) {
+    answer += num;
+  }
+  return answer;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let answer = 1;
+  for (let num of array) {
+    answer *= num;
+  }
 
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	let answer = 1;
+
+  for (let i = num1; i >= 1; i--) {
+    answer *= i;
+    console.log(i);
+  }
+
+    console.log(answer);
+  return answer;
 };
 
 // Do not edit below this line
