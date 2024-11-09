38edee07dd92a69a239ba36acdb1580837848b0f
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-09 12:06:26 -0500

    Completed tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let answer = ((fahrenheit - 32) * (5/9));
+  let answerRounded = Math.round(answer);
+  if (answer == answerRounded) {
+    return answer;
+  } else {
+    return Number(answer.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let answer = ((celsius * (9/5)) + 32);
+  let answerRounded = Math.round(answer);
+  if (answer == answerRounded) {
+    return answer;
+  } else {
+    return Number(answer.toFixed(1));
+  }
 };
+
+convertToCelsius(100);
 
 // Do not edit below this line
 module.exports = {
