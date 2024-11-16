d7a15401ee8da105687bd561d66396c1026ff6e6
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-16 12:09:45 -0500

    Completed findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,55 @@
-const findTheOldest = function() {
+const findTheOldest = function(objectArray) {
+    
+    let oldestName = "";
+    let oldestAge = 0;
+    let oldestPerson = {};
+    let currentYear = new Date().getFullYear();
+    
+    for (let item of objectArray) {
 
+        if (item.yearOfDeath === undefined) {
+            if (currentYear - item.yearOfBirth > oldestAge) {              
+                oldestAge = (currentYear - item.yearOfBirth);
+                oldestName = item.name;
+                oldestPerson = item;
+                continue;
+            } else {
+                continue;
+            }
+        }
+
+        if ((item.yearOfDeath - item.yearOfBirth) > oldestAge) {
+            oldestAge = (item.yearOfDeath - item.yearOfBirth);
+            oldestName = item.name;
+            oldestPerson = item;
+            continue;
+        } else {
+            continue;
+        }
+    }
+
+    return oldestPerson;
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 2018
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+]
+
+
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
