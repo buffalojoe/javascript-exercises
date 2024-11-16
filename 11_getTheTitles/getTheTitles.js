be62e18477f3ad77627daef16e57cce0893d2ed5
Joe <42217874+buffalojoe@users.noreply.github.com>
2024-11-16 11:52:16 -0500

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const getTheTitles = function(objArray) {
+    let bookTitles = [];
 
+    for (let item of objArray) {
+        bookTitles.push(item.title);
+    }
+
+    console.log(bookTitles);
+    return bookTitles;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
